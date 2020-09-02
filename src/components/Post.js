bfa64f6bda8a9cb5b681fde607769569ac138dbf
Manny Singh <42702278+mannysinghh11@users.noreply.github.com>
2020-09-02 01:22:40 -0700

    Small UI changes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import ShareIcon from '@material-ui/icons/Share';
+
 import profilePicture2 from '../assets/images/profilePicture2.jpg'
 
 import styles from '../styles/Post.css'
@@ -21,6 +25,24 @@ class Post extends React.Component {
 
             <div className = {styles.postContent}>
                 <p className = {styles.post}>{this.props.content}</p>
+            </div>
+
+            <div className = {styles.footer}>
+                <div className = {styles.iconHolder}>
+                    <ThumbUpIcon className={styles.icon}/>
+                    <p>Like</p>
+                </div>
+
+                <div className = {styles.iconHolder}>
+                    <ChatBubbleIcon className={styles.icon}/>
+                    <p>Comment</p>
+                </div>
+
+                <div className = {styles.iconHolder}>
+                    <ShareIcon className={styles.icon}/>
+                    <p>Share</p>
+                </div>
+
             </div>
     	</div>
     );
